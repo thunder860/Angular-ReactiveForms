8a3844bc045a067ba0fa1fb911268c15139c927b
Bobby Wisniewski <bobbywisniewski@amrock.com>
2019-02-11 09:21:17 -0500

    end module 'Dynamically Duplicate input elements'

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -1,27 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
 
 import { debounceTime } from 'rxjs/operators';
 
 import { Customer } from './customer';
 
-function ratingRange(min: number, max: number): ValidatorFn { 
+function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean } | null => {
-  if (c.value != null && (isNaN(c.value) || c.value < min || c.value > max)) {
-    return { 'range': true };
+    if (c.value != null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { 'range': true };
+    }
+    return null;
   }
-  return null;
-}}
+}
 
 function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
   const emailControl = c.get('email');
   const confirmControl = c.get('confirmEmail');
 
-  if(emailControl.pristine || confirmControl.pristine) {
+  if (emailControl.pristine || confirmControl.pristine) {
     return null;
   }
 
-  if(emailControl.value === confirmControl.value) {
+  if (emailControl.value === confirmControl.value) {
     return null;
   }
 
@@ -37,6 +38,10 @@ export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
   emailMessage: string;
+
+  get addresses(): FormArray {
+    return <FormArray>this.customerForm.get('addresses');
+  }
 
   private validationMessages = {
     required: 'Please enter your email address.',
@@ -56,18 +61,19 @@ export class CustomerComponent implements OnInit {
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
-      sendCatalog: false
+      sendCatalog: true,
+      addresses: this.fb.array([ this.buildAddress() ])
     });
 
     this.customerForm.get('notification').valueChanges.subscribe(
-        value => this.setNotification(value)
+      value => this.setNotification(value)
     );
 
     const emailControl = this.customerForm.get('emailGroup.email');
     emailControl.valueChanges.pipe(
-        debounceTime(1000)
-      ).subscribe(
-        value => this.setMessage(emailControl)
+      debounceTime(1000)
+    ).subscribe(
+      value => this.setMessage(emailControl)
     );
   }
 
@@ -85,7 +91,7 @@ export class CustomerComponent implements OnInit {
 
   setNotification(notifyVia: string): void {
     const phoneControl = this.customerForm.get('phone');
-    if(notifyVia === 'text') {
+    if (notifyVia === 'text') {
       phoneControl.setValidators(Validators.required);
     } else {
       phoneControl.clearValidators();
@@ -97,7 +103,22 @@ export class CustomerComponent implements OnInit {
     this.emailMessage = '';
     if ((c.touched || c.dirty) && c.errors) {
       this.emailMessage = Object.keys(c.errors).map(
-        key => this.emailMessage += this.validationMessages[key]).join(' ');      
+        key => this.emailMessage += this.validationMessages[key]).join(' ');
     }
+  }
+
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
+
+  buildAddress() : FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
+    });
   }
 }
